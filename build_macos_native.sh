dd2cc00c8768836218edbf60c8d321009e517ba4
Sebastian Annies <Sebastian.Annies@googlemail.com>
2020-03-10 10:16:58 +0100

    build MacOS

---
+++ build_macos_native.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+export MACOSX_DEPLOYMENT_TARGET=10.10
+
+brew install pkg-config
+brew install freetype
+brew install zlib
+
+builddir=$PWD
+
+mkdir -p bin/mac
+mkdir -p build
+
+pushd build
+cmake -DCMAKE_BUILD_TYPE=Release -DTSMUXER_GUI=TRUE ..
+make
+
+pushd tsMuxerGUI
+macdeployqt tsMuxerGUI.app -verbose=3
+popd
+
+mv tsMuxer/tsmuxer "${builddir}/bin/mac/tsMuxeR"
+mv tsMuxerGUI/tsMuxerGUI.app "${builddir}/bin/mac"
+popd
+
+zip -r ./bin/mac.zip ./bin/mac
+ls ./bin/mac/tsMuxeR && ls ./bin/mac/tsMuxerGUI.app/Contents/MacOS/tsMuxerGUI && ls ./bin/mac.zip
